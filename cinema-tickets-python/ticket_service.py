fa55c5f1c531f04613d6508a9b6a18ee63d37511
Richard Parke <richardparke15@gmail.com>
2022-10-17 14:14:11 +0100

    added validation logic

--- cinema-tickets-python/ticket_service.py
+++ cinema-tickets-python/ticket_service.py
@@ -1,15 +1,45 @@
 
 from purchase_exceptions import InvalidPurchaseException
-
+from ticket_type_request import TicketTypeRequest
 
 class TicketService:
+
 
     """
 
       purchase_tickets should be the only public method
 
     """
+    _MAX_NUMBER_OF_TICKETS=20
 
-    def purchase_tickets(account_id=None, ticket_type_requests=[]):
+    def _validate_account_id(self, account_id):
+      if not isinstance(account_id, int):
+        raise InvalidPurchaseException("Not an Integer")
 
-        raise InvalidPurchaseException
+    def _validate_ticket_type_requests(self, ticket_type_requests):
+      for ticket_type_request in ticket_type_requests:
+        if not isinstance(ticket_type_request, TicketTypeRequest):
+          raise InvalidPurchaseException("Not a Valid Ticket Type Request")
+    
+    def _validate_number_of_tickets(self, ticket_type_requests):
+      total_number_of_tickets = sum([ticket_type_request.number_of_tickets for ticket_type_request in ticket_type_requests])
+      if total_number_of_tickets > self._MAX_NUMBER_OF_TICKETS:
+        raise InvalidPurchaseException("You can only purchase 20 tickets")
+
+    def _validate_request(self, account_id, ticket_type_requests):
+        self._validate_ticket_type_requests(ticket_type_requests)
+        self._validate_account_id(account_id)
+        self._validate_number_of_tickets(ticket_type_requests)
+        self._verify_that_children_and_infants_are_supervised(ticket_type_requests)
+
+    def _verify_that_children_and_infants_are_supervised(self, ticket_type_requests):
+      list_of_ticket_types = [ticket_type_request.ticket_type for ticket_type_request in ticket_type_requests]
+      if "ADULT" not in list_of_ticket_types:
+        raise InvalidPurchaseException("An Adult is Required in the Party")
+
+    def purchase_tickets(self, account_id=None, ticket_type_requests=[]):
+        self._validate_request(account_id, ticket_type_requests)
+
+# # debugging code
+# service = TicketService()
+# service.purchase_tickets(20, [TicketTypeRequest("ADULT",3), TicketTypeRequest("CHILD", 20)])
