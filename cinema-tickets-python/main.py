ba52aab07bb82cddcc52cbd9e3ff57450e9c9f44
Richard Parke <richardparke@Richards-MacBook-Air.local>
2022-10-17 19:53:50 +0100

    refactored main

--- cinema-tickets-python/main.py
+++ cinema-tickets-python/main.py
@@ -1,9 +1,10 @@
 
 from ticket_service import TicketService
-
+from paymentgateway.ticket_payment_service import TicketPaymentService
+from seatbooking.seat_reservation_service import SeatReservationService
 
 if __name__ == '__main__':
 
-    ticket_service = TicketService()
+    ticket_service = TicketService(SeatReservationService(), TicketPaymentService())
 
     ticket_service.purchase_tickets()
