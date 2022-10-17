34d918366278a9596afd6d53b50d0e78eb7fbd23
Richard Parke <richardparke@Richards-MacBook-Air.local>
2022-10-17 19:52:56 +0100

    working implementation with payment services factored out

--- cinema-tickets-python/test/test_ticket_service.py
+++ cinema-tickets-python/test/test_ticket_service.py
@@ -1,26 +1,28 @@
 from ticket_service import TicketService
 from ticket_type_request import TicketTypeRequest
 from purchase_exceptions import InvalidPurchaseException
+from paymentgateway.ticket_payment_service import TicketPaymentService
+from seatbooking.seat_reservation_service import SeatReservationService
 import pytest
 
 
 #Test Input Validation
 def test_account_id_fails_for_non_int():
-    service = TicketService()
+    service = TicketService(SeatReservationService(), TicketPaymentService())
     with pytest.raises(InvalidPurchaseException):
         service.purchase_tickets("Not an Integer" , [TicketTypeRequest("ADULT", 3)])
 
 def test_ticket_type_requests_fails_for_wrong_type():
-    service = TicketService()
+    service = TicketService(SeatReservationService(), TicketPaymentService())
     with pytest.raises(InvalidPurchaseException):
         service.purchase_tickets(20 , [TicketTypeRequest("ADULT", 3), "String, not a TicketTypeRequest"])
 
 def test_ticket_type_requests_fails_if_adult_absent():
-    service = TicketService()
+    service = TicketService(SeatReservationService(), TicketPaymentService())
     with pytest.raises(InvalidPurchaseException):
         service.purchase_tickets(20, [TicketTypeRequest("CHILD", 3)])
 
 def test_ticket_type_requests_fails_if_number_of_tickets_greater_than_twenty():
-    service = TicketService()
+    service = TicketService(SeatReservationService(), TicketPaymentService())
     with pytest.raises(InvalidPurchaseException):
         service.purchase_tickets(20, [TicketTypeRequest("ADULT", 200)])
--- cinema-tickets-python/ticket_service.py
+++ cinema-tickets-python/ticket_service.py
@@ -1,16 +1,16 @@
+from paymentgateway.ticket_payment_service import TicketPaymentService
 from purchase_exceptions import InvalidPurchaseException
 from ticket_type_request import TicketTypeRequest
 from seatbooking.seat_reservation_service import SeatReservationService
 
 class TicketService:
 
-
-    """
-
-      purchase_tickets should be the only public method
-
-    """
     _MAX_NUMBER_OF_TICKETS=20
+    _TICKET_PRICES = {"ADULT":20, "CHILD":10, "INFANT":0}
+
+    def __init__(self, seat_reservation_service, ticket_payment_service):
+      self.seat_reservation_service = seat_reservation_service
+      self.ticket_payment_service = ticket_payment_service
 
     def _validate_account_id(self, account_id):
       if not isinstance(account_id, int):
@@ -38,15 +38,21 @@ class TicketService:
         self._verify_that_children_and_infants_are_supervised(ticket_type_requests)
     
     def _make_seat_reservation(self, account_id, ticket_type_requests):
+      total_seats = sum([ticket_type_request.number_of_tickets for ticket_type_request in ticket_type_requests if ticket_type_request.get_ticket_type() != "INFANT"])
+      self.seat_reservation_service.reserve_seat(account_id, total_seats)
+      print(total_seats)
 
-      total_seats = sum([ticket_type_request.number_of_tickets for ticket_type_request in ticket_type_requests])
-      seat_reservtion_service = SeatReservationService()
-      seat_reservtion_service.reserve_seat(account_id, total_seats)
+    def _purchase_tickets(self, account_id, ticket_type_requests):
+      total_price = sum([self._TICKET_PRICES[ticket_type_request.get_ticket_type()] for ticket_type_request in ticket_type_requests])
+      self.ticket_payment_service.make_payment(account_id, total_price)
+      print(total_price)
 
     def purchase_tickets(self, account_id=None, ticket_type_requests=[]):
         self._validate_request(account_id, ticket_type_requests)
         self._make_seat_reservation(account_id, ticket_type_requests)
+        self._purchase_tickets(account_id, ticket_type_requests)
+
 
 # # debugging code
-# service = TicketService()
-# service.purchase_tickets(20, [TicketTypeRequest("ADULT",3), TicketTypeRequest("CHILD", 20)])
+service = TicketService(SeatReservationService(), TicketPaymentService())
+service.purchase_tickets(20, [TicketTypeRequest("ADULT",3), TicketTypeRequest("CHILD", 2), TicketTypeRequest("INFANT", 3)])
